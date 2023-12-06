050daee8aeda5986473ba69b21d6fa856458cda0
DomingoOvando <108771155+DomingoOvando@users.noreply.github.com>
2023-12-06 13:57:04 -0600

    Update Tarea #990 Crear un jail, del estilo de bandit.py
    Tarea completada

--- Tarea #990 Crear un jail, del estilo de bandit.py
+++ Tarea #990 Crear un jail, del estilo de bandit.py
@@ -4,10 +4,11 @@ import os
 import builtins
 import math
 import random
+@author OVANDO CUPUL JOSE DOMINGO
 
 class RestrictedEnvironment:
     def __init__(self):
-        # Limitar las funciones incorporadas
+        # Limitar todas las funcionaliades que se ingresan en el script
         self.safe_builtins = {
             'print': print,
             'input': input,
@@ -25,16 +26,13 @@ class RestrictedEnvironment:
             'max': max,
         }
 
-        # Limitar el acceso al sistema de archivos
         self.safe_builtins['open'] = self.restricted_open
 
-        # Limitar el acceso a algunos módulos
         self.safe_modules = {
             'math': math,
             'random': random,
         }
 
-        # Variables globales disponibles para el usuario
         self.user_globals = {}
 
     def restricted_open(self, *args, **kwargs):
@@ -53,21 +51,17 @@ def main():
 
     while True:
         try:
-            # Leer el código del usuario
             user_code = input(">>> ")
 
-            # Salir si el usuario escribe 'exit'
             if user_code.lower() == 'exit':
                 break
 
-            # Ejecutar el código proporcionado por el usuario en el entorno restringido
             result = environment.execute_code(user_code)
             print(result)
         except KeyboardInterrupt:
             print("\nSaliendo...")
             break
         except Exception as e:
-            # Capturar excepciones para que no se interrumpa el programa principal
             print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
